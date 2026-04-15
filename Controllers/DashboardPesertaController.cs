8579ce009c5759786996b0c40c00d74a02c755e5
FebriantiPujiatiningsih <fpujiatiningsih@gmail.com>
2026-04-15 09:48:16 +0700

    perbaikan

--- Controllers/DashboardPesertaController.cs
+++ Controllers/DashboardPesertaController.cs
@@ -67,24 +67,195 @@ namespace sinta_asp.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetInformasiTersedia()
+        public IActionResult GetInformasiTersedia()
         {
-            var data = await _context.PendaftaranMagang
-                .Where(m => !string.IsNullOrEmpty(m.Company) 
-                        && !string.IsNullOrEmpty(m.Region) 
-                        && !string.IsNullOrEmpty(m.Lokasi))
-                .Select(m => new
+            var data = new List<object>();
+
+            // =========================
+            // MASTER DATA MAGANG - KPI
+            // =========================
+            var dataKPI = new Dictionary<string, List<string>>
+            {
                 {
-                    company = m.Company,
-                    region = m.Region,
-                    lokasi = m.Lokasi,
-                    createdAt = m.CreatedAt
-                })
-                .Distinct()
-                .OrderBy(x => x.company)
-                .ThenBy(x => x.region)
-                .ThenBy(x => x.lokasi)
-                .ToListAsync();
+                    "Refinery Unit VI Balongan", new List<string>
+                    {
+                        "Akuntansi / Ekonomi & Bisnis",
+                        "Elektro (Arus Kuat)",
+                        "Elektro (Arus Lemah)",
+                        "Emergency & Insurance",
+                        "Health",
+                        "Hukum",
+                        "Ilmu Komunikasi / FISIP / Administrasi Publik",
+                        "Internal Audit",
+                        "Kelautan / Perkapalan",
+                        "Kimia Murni / MIPA",
+                        "Konversi Energi / Migas / Kimia Air Bersih / Blanding / Loading",
+                        "Logistik / Pergudangan / Procurement",
+                        "Manajemen / SDM / Psikologi",
+                        "Metalurgi / Material / Dirgantara",
+                        "Safety (K3) / SMK3",
+                        "Teknik Fisika",
+                        "Teknik Industri",
+                        "Teknik Informatika",
+                        "Teknik Kimia",
+                        "Teknik Lingkungan",
+                        "Teknik Mesin",
+                        "Teknik Mesin (Rotating)",
+                        "Teknik Sipil"
+                    }
+                }
+            };
+
+            // =========================
+            // MASTER DATA MAGANG - PPN
+            // =========================
+            var dataPPN = new Dictionary<string, List<string>>
+            {
+                {
+                    "Regional Jatimbalinus", new List<string>
+                    {
+                        "Asset Operation MOR V","Bitumen Plant Gresik","C&T IA Jatimbalinus","Comm, Rel, & CSR MOR V",
+                        "Corporate Operation & Service Region V","Corporate Sales Region V","DPPU BIL","DPPU Eltari Group",
+                        "DPPU Iswahyudi","DPPU Juanda","DPPU Ngurah Rai","Finance MOR V","Fuel Terminal Atapupu",
+                        "Fuel Terminal Badas","Fuel Terminal Bima","Fuel Terminal Camplong","Fuel Terminal Ende",
+                        "Fuel Terminal Kalabahi","Fuel Terminal Madiun","Fuel Terminal Malang","Fuel Terminal Maumere",
+                        "Fuel Terminal Reo","Fuel Terminal Sanggaran","Fuel Terminal Tenau","Fuel Terminal Tuban",
+                        "Fuel Terminal Waingapu","HC Jatimbalinus","HSSE Region V","Integrated Terminal Ampenan",
+                        "Integrated Terminal Manggis","Integrated Terminal Surabaya","Integrated Terminal T. Wangi",
+                        "Legal Counsel Regional Jatimbalinus","Marine Region V","Medical Jatimbalinus",
+                        "Procurement MOR V","Rel & Project Dev Region V","Retail Bali","Retail Kediri",
+                        "Retail Malang","Retail NTB","Retail NTT","Retail Sales Region V","Retail Surabaya",
+                        "S&D Region V","SSC ICT VI Jatimbalinus","XXX"
+                    }
+                },
+                {
+                    "Regional Jawa Bagian Barat", new List<string>
+                    {
+                        "Asset Operation JBB","Corp. Opt & Serv JBB","Corporate Sales JBB","DPPU Halim PK Group",
+                        "DPPU Husein Sastranegara","DPPU Kertajati","Finance JBB","Fuel Terminal Bandung Group",
+                        "Fuel Terminal Cikampek","Fuel Terminal Tasikmalaya","Fuel Terminal Tg Gerem","HSSE JBB",
+                        "Human Capital","Integrated Terminal Balongan","Integrated Terminal Jakarta","Legal Counsel JBB",
+                        "Medical JBB","MWH & LPG Cylinder","Procurement JBB","Reliability & Project Dev JBB",
+                        "SA Retail Bandung","SA Retail Cirebon","SA Retail Karawang","SA Retail Sukabumi",
+                        "SAM Retail Banten","SAM Retail Jabode","SHAFTHI","SHIPS","SCC ICT JBB",
+                        "Supply & Distribution JBB","Unit Comm, Rel & CSR JBB"
+                    }
+                },
+                {
+                    "Regional Jawa Bagian Tengah", new List<string>
+                    {
+                        "AFT Adi Sumarmo","AFT Adi Sucipto","AFT Ahmad Yani","AFT YIA",
+                        "Fuel Terminal Boyolali","Fuel Terminal Lomanis","Fuel Terminal Maos",
+                        "Fuel Terminal Rewulu","Fuel Terminal Tegal","Integrated Terminal Cilacap",
+                        "Integrated Terminal Semarang","Kantor Branch Marketing DIY & Surakarta",
+                        "Kantor Unit - Asset Operation JBT","Kantor Unit - Comm, Rel & CSR JBT",
+                        "Kantor Unit - Corp Operation & Serv JBT","Kantor Unit - Corporate Sales JBT",
+                        "Kantor Unit - Finance JBT","Kantor Unit - HC JBT","Kantor Unit - HSSE JBT",
+                        "Kantor Unit - Internal Audit","Kantor Unit - Legal Counsel JBT",
+                        "Kantor Unit - Medical JBT","Kantor Unit - Operational Risk JBT",
+                        "Kantor Unit - Procurement JBT","Kantor Unit - Rel & Project Dev JBT",
+                        "Kantor Unit - Retail Sales JBT","Kantor Unit - SSC ICT V JBT",
+                        "Kantor Unit - Supply & Distribution JBT"
+                    }
+                },
+                {
+                    "Regional Kalimantan", new List<string>
+                    {
+                        "DPPU APT Pranoto","DPPU H. Asan","DPPU Iskandar","DPPU Juwata",
+                        "DPPU Kalimaru","DPPU Sepinggan","DPPU Supadio","DPPU Syamsudin Noor",
+                        "DPPU Tjilik Riwut","Fuel Terminal Pulang Pisau","Fuel Terminal Kotabaru",
+                        "Fuel Terminal Pangkalan Bun","Fuel Terminal Samarinda","Fuel Terminal Sampit",
+                        "Fuel Terminal Sintang","Fuel Terminal Tarakan","Integrated Terminal Balikpapan",
+                        "Integrated Terminal Banjarmasin","Integrated Terminal Pontianak",
+                        "Kantor Patra Niaga Region Kalimantan","SAM Retail Kalbar","SAM Retail Kalselteng",
+                        "SAM Retail Kaltimut"
+                    }
+                },
+                {
+                    "Regional Maluku Papua", new List<string>
+                    {
+                        "Aviation FT Babullah","Aviation FT Deo","Aviation FT Depati Mopah",
+                        "Aviation FT Depati Rendani","Aviation FT Dumatubun","Aviation FT Frans Kaisiepo",
+                        "Aviation FT Mathilda","Aviation FT Mozes Kilangin","Aviation FT Paniai",
+                        "Aviation FT Pattimura","Aviation FT Sentani","Aviation FT Utarom",
+                        "FT Biak","FT Bula","FT Dobo","FT Fak-Fak","FT Kaimana","FT Labuha",
+                        "FT Manokwari","FT Masohi","FT Merauke","FT Nabire","FT Namlea",
+                        "FT Sanana","FT Saumlaki","FT Serui","FT Sorong","FT Ternate",
+                        "FT Tobelo","FT Tual","IT Jayapura","IT Wayame",
+                        "Kantor Region - Asset Operation Papua-Maluku",
+                        "Kantor Region - Comm, Rel & CSR Papua-Maluku",
+                        "Kantor Region - Corp Operation & Serv Papua-Maluku",
+                        "Kantor Region - Corporate Sales Papua-Maluku",
+                        "Kantor Region - Finance Papua-Maluku",
+                        "Kantor Region - HC Papua-Maluku",
+                        "Kantor Region - HSSE Papua-Maluku",
+                        "Kantor Region - Legal Counsel Papua-Maluku",
+                        "Kantor Region - Medical Papua-Maluku",
+                        "Kantor Region - Procurement Papua-Maluku",
+                        "Kantor Region - Rel & Project Dev Papua-Maluku",
+                        "Kantor Region - Retail Sales Papua-Maluku",
+                        "Kantor Region - Supply & Dist Papua-Maluku",
+                        "Sales Area Ambon"
+                    }
+                },
+                {
+                    "Regional Sumbagut", new List<string>
+                    {
+                        "Asset Operation Region Sumbagut","Branch Marketing Aceh",
+                        "Branch Marketing Kepulauan Riau","Branch Marketing Sibolga",
+                        "Branch Marketing Sumbar","Communication & CSR Region Sumbagut",
+                        "Corp Operation & Serv Region Sumbagut","Corporate Sales Region Sumbagut",
+                        "DPPU Hang Nadim Group","DPPU Kualanamu Group","DPPU Minangkabau",
+                        "DPPU SIM","DPPU SSK II","Finance Region Sumbagut",
+                        "Fuel Terminal Batam","Fuel Terminal Gunung Sitoli",
+                        "Fuel Terminal Kijang Group","Fuel Terminal Kisaran",
+                        "Fuel Terminal Krueng Raya","Fuel Terminal Medan Group",
+                        "Fuel Terminal Meulaboh","Fuel Terminal Natuna Group",
+                        "Fuel Terminal Pematang Siantar","Fuel Terminal Sabang",
+                        "Fuel Terminal Sei Siak","Fuel Terminal Sibolga",
+                        "Fuel Terminal Simeulue","Fuel Terminal Tembilahan",
+                        "HC Region Sumbagut","HSSE Region Sumbagut",
+                        "IA Region I","Integrated Terminal Dumai",
+                        "Integrated Terminal Lhokseumawe","Integrated Terminal Tanjung Uban",
+                        "Integrated Terminal Teluk Kabung","Legal Counsel Region Sumbagut",
+                        "Medical Region Sumbagut","Procurement Region Sumbagut",
+                        "Rel & Project Dev Region Sumbagut",
+                        "Retail Sales Region Sumbagut",
+                        "SSC ICT I Region Sumbagut",
+                        "Supply & Distribution Region Sumbagut"
+                    }
+                }
+            };
+
+            foreach (var region in dataKPI)
+            {
+                foreach (var posisi in region.Value)
+                {
+                    data.Add(new
+                    {
+                        companyCode = "KPI",
+                        companyName = "PT Kilang Pertamina Internasional",
+                        region = region.Key,
+                        lokasi = posisi,
+                        status = "Tersedia"
+                    });
+                }
+            }
+
+            foreach (var region in dataPPN)
+            {
+                foreach (var posisi in region.Value)
+                {
+                    data.Add(new
+                    {
+                        companyCode = "PPN",
+                        companyName = "PT Pertamina Patra Niaga",
+                        region = region.Key,
+                        lokasi = posisi,
+                        status = "Tersedia"
+                    });
+                }
+            }
 
             return Json(data);
         }
